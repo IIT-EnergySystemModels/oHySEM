8166418980fc1e8d6d176165ec65a84520cc597b
erikfilias <erikfilias@gmail.com>
2024-09-06 12:46:00 +0200

    Create __init__.py

---
+++ oHySEM/__init__.py
@@ -0,0 +1,17 @@
+"""
+Optimized Hybrid Systems for Energy and Market management (oHySEM)
+
+
+    Args:
+        case:   Name of the folder where the CSV files of the case are found
+        dir:    Main path where the case folder can be found
+        solver: Name of the solver
+
+    Returns:
+        Output results in CSV files that are found in the case folder.
+
+    Examples:
+        >>> import oHySEM as oH
+        >>> oH.routine("9n", "C:\\Users\\UserName\\Documents\\GitHub\\oHySEM", "glpk")
+"""
+__version__ = "0.1.1"
