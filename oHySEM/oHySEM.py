6064b44fe54fa142f8566d7113b84d388e2fc5b2
erikfilias <erikfilias@gmail.com>
2024-09-11 10:47:58 +0200

    Update oHySEM.py

--- oHySEM/oHySEM.py
+++ oHySEM/oHySEM.py
@@ -32,7 +32,7 @@ from   colour            import Color
 for i in range(0, 117):
     print('-', end="")
 
-print('\nSoftware for Optimizing Hybrid Systems for Energy and Market management (oHySEM) - Version 1.0.0 - September 09, 2024')
+print('\nSoftware for Optimizing Hybrid Systems for Energy and Market management (oHySEM) - Version 1.0.4 - September 11, 2024')
 print('#### Non-commercial use only ####')
 
 parser = argparse.ArgumentParser(description='Introducing main arguments...')
